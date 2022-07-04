023ba8b3c5253807793911a559fc9a9829fd3edf
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-04 15:12:06 +0200

    shopping cart desktop view

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -10,31 +10,40 @@ const Wrapper = styled.div`
 
   @media screen and (min-width: 768px) {
     width: 65vw;
-    margin-left: 16rem;
+    margin-left: 33vw;
+    margin-top: 2rem;
+
+    h1 {
+      font-size: 1.75rem;
+    }
   }
 `;
 
 const CartItemsWrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    margin-top: 4rem;
+  display: flex;
+  flex-direction: column;
+  margin-top: 4rem;
 
-    @media screen and (min-width: 768px) {
-        flex-direction: row;
-        flex-wrap: wrap;
-    }
+  @media screen and (min-width: 768px) {
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
 `;
 
 const CartItem = styled.article`
   display: flex;
+
+  &:nth-child(n + 2) {
+    margin-top: 4rem;
+  }
 `;
 
 const Details = styled.div`
-    margin-left: 1rem;
+  margin-left: 1rem;
 
   h2 {
     color: ${({ theme }) => theme.colors.black};
-    margin-top: .5rem;
+    margin-top: 0.5rem;
   }
 
   p {
@@ -43,32 +52,86 @@ const Details = styled.div`
 `;
 
 const ImgWrapper = styled.div`
+  img {
+    width: 40vw;
+    height: 24vh;
+    object-fit: cover;
+  }
+
+  @media screen and (min-width: 768px) {
     img {
-      width: 40vw;
-      height: 24vh;
-      object-fit: cover;
+      width: 17vw;
+      height: 17vh;
     }
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const Checkout = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 5rem auto;
+  background-color: ${({ theme }) => theme.colors.orange};
+  width: 100vw;
+  height: 30vh;
+  color: ${({ theme }) => theme.colors.white};
+  padding: 1rem;
+
+  div {
+    display: flex;
+    align-items: center;
+
+    span {
+        margin-left: 2rem;
+    }
+  }
 `;
 
 const ShoppingCart = () => {
   return (
-    <Wrapper>
-      <h1>ShoppingCart</h1>
-      <CartItemsWrapper>
-        <CartItem>
-          <ImgWrapper>
-            <img
-              src="https://images.unsplash.com/photo-1567423285116-c31e6a93e939?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
-              alt=""
-            />
-          </ImgWrapper>
-          <Details>
-            <h2>Name</h2>
-            <p>Price</p>
-          </Details>
-        </CartItem>
-      </CartItemsWrapper>
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <h1>ShoppingCart</h1>
+        <CartItemsWrapper>
+          <CartItem>
+            <ImgWrapper>
+              <img
+                src="https://images.unsplash.com/photo-1567423285116-c31e6a93e939?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
+                alt=""
+              />
+            </ImgWrapper>
+            <Details>
+              <h2>Name</h2>
+              <p>Price</p>
+            </Details>
+          </CartItem>
+          <CartItem>
+            <ImgWrapper>
+              <img
+                src="https://images.unsplash.com/photo-1567423285116-c31e6a93e939?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
+                alt=""
+              />
+            </ImgWrapper>
+            <Details>
+              <h2>Name</h2>
+              <p>Price</p>
+            </Details>
+          </CartItem>
+        </CartItemsWrapper>
+      </Wrapper>
+      <Checkout>
+        <div>
+            <h3>Total</h3>
+            <span>$122</span>
+        </div>
+        <button>Order now!</button>
+      </Checkout>
+    </Container>
   );
 };
 
